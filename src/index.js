cf75717f7664665d92cfd28d6157016f7d6162b1
theodora90 <theodora@lifebank.ng>
2022-06-09 08:20:49 +0100

    Solution

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 // complete the function
-function prime (num) {
+function prime(num) {
   // code goes here
+  const boundary = Math.floor(Math.sqrt(num));
+  for (var i = 2; i <= boundary; i++) if (num % i === 0) return false;
+  return num >= 2;
 }
 
-function solution (arg) {
-  return prime(arg);
+function solution() {
+  return prime(17);
 }
 
 if (typeof require !== "undefined" && require.main === module) {
